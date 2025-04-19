346546a7e332767d9182a65d7a3d88c88524c82b
Kishan <kishan1292001@gmail.com>
2025-04-19 17:39:25 +0100

    First Final Change

--- scripts/script.js
+++ scripts/script.js
@@ -518,4 +518,15 @@ particlesJS('particles-js', {
     }
 });
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/scripts/service-worker.js')
+            .then(registration => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, err => {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
+}
+
 
